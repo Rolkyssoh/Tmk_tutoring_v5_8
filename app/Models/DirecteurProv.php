40968957c06fd920a498ee6f5fa9096fcd0b32d9
Rolkyssoh <juventussoh@gmail.com>
2019-11-05 14:07:43 +0100

    first commit

--- app/Models/DirecteurProv.php
+++ app/Models/DirecteurProv.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class DirecteurProv extends Authenticatable
 {
-    use Notifiable;
+    // use Notifiable;
 
     protected $guard = 'directeur_prov';
 
